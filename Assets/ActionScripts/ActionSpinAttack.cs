5e9dff46d1af90835645e7263ad7b44ae9ec9cc8
v <v@vs-MacBook-Pro.local>
2017-11-07 23:36:06 -0500

    wip

---
+++ Assets/ActionScripts/ActionSpinAttack.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Linq;
+
+public class ActionSpinAttack : Action
+{
+    private int targetsToResolve = 0;
+
+    public override string Name()
+    {
+        return ("Spin Attack");
+    }
+
+    public override string Description()
+    {
+        return ("Attack all units in adjacent tiles");
+    }
+
+    public override void ReceiveVisualFeedback(Cursor cursor)
+    {
+        if (cursor.standingUnit)
+        {
+            cursor.standingUnit.ReceiveDamage(15);
+            targetsToResolve -= 1;
+        }
+        if(targetsToResolve == 0) Unit().FinishAction();
+    }
+
+    public override int MaxDistance()
+    {
+        return (0);
+    }
+
+    public override bool CanTargetSelf()
+    {
+        return (true);
+    }
+
+    public override int RadialDistance()
+    {
+        return (1);
+    }
+
+    protected override void DoAction(Cursor cursor)
+    {
+        List<Cursor> allTiles = Helpers.GetRadialTiles(cursor.xPos, cursor.zPos, this.RadialDistance(), true);
+        List<Cursor> tiles = allTiles.Where(tile =>
+        {
+            return tile.standingUnit && tile.standingUnit != Unit();
+        }).ToList();
+        targetsToResolve += tiles.Count;
+
+        foreach (Cursor tile in tiles)
+        {
+            CreateVisual(tile, tile.transform.position);
+        }
+    }
+}
--- Assets/Unit.cs
+++ Assets/Unit.cs
@@ -121,7 +121,7 @@ public class Unit: NetworkBehaviour {
       GameObject instance;
 
       List<string> actionList = new List<string> {
-        "Punish", "Fire", "Razz", "LightningStab"
+        "Punish", "Fire", "SpinAttack", "LightningStab"
       };
 
       List<string> stanceList = new List<string> {
